c5c0b78606d4feac155131eaebd7785753b5ec53
Hawkerh <cissealbert.d@icloud.com>
2024-02-26 10:39:08 +0100

    init

---
+++ lib/Moderable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Moderable/version"
+
+module Moderable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Moderable_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Moderable do
+  it "has a version number" do
+    expect(Moderable::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
